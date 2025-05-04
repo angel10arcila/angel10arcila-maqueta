c30a7582c3fb2eaec40a61577dbc98bb56e7a9d8
angel10arcila <131724057+angel10arcila@users.noreply.github.com>
2025-05-04 14:36:46 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const mediaFiles = [
      {
          name: "Llanerato - Ángel José Arcila Parra",
          url: "https://raw.githubusercontent.com/angel10arcila/angeljosearcilaparra/refs/heads/main/El Llanerato V3 - Letra y Música de Ángel José Arcila Parra .mp3",
-         type: "audio"
+         type: "audio",
      },
                  
 ];
